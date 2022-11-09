8580a46a52c606c83f119a58fe38c4207919f29c
Adam Ramdan <79040246+ar-kun@users.noreply.github.com>
2022-11-09 11:23:44 +0700

    Update config.js

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  KEY: 'YOUR_API_KEY',
+  KEY: '965dbd3e2ec880a61eabf82459435d96',
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
